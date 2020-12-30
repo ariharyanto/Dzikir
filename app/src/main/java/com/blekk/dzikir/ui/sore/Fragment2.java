e8c82bc0e651cdb4e7399d414d3c06b40b62b0a4
Ari Haryanto <ariharyanto.fw@noreply.github.com>
2020-12-31 02:20:39 +0700

    Initial commit

---
+++ app/src/main/java/com/blekk/dzikir/ui/sore/Fragment2.java
@@ -0,0 +1,40 @@
+package com.blekk.dzikir.ui.sore;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.blekk.dzikir.R;
+
+public class Fragment2 extends Fragment {
+
+    String url = "file:///android_asset/webview2.html";
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View v=inflater.inflate(R.layout.fragment_sore, container, false);
+        WebView mWebView = (WebView) v.findViewById(R.id.webView);
+        mWebView.loadUrl(url);
+
+        // Enable Javascript
+        WebSettings webSettings = mWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        // Force links and redirects to open in the WebView instead of in a browser
+        mWebView.setWebViewClient(new WebViewClient());
+
+        return v;
+    }}
